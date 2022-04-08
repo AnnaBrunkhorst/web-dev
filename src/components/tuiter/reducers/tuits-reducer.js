90e5a2b97e558c24abc33623fc6a98088595e3be
Anna Brunkhorst <abrunkhorst510@gmail.com>
2022-04-08 17:16:07 -0400

    a8 cors issue

--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,53 +1,64 @@
-import tuits from '../data/tuits.json';
+// import tuits from '../data/tuits.json';
 
-const tuitsReducer =
-    (state = tuits, action) => {
-      switch (action.type) {
-        case 'like-tuit':
-          return state.map(post => {
-            if(post.id === action.post.id) {
-              if(post.liked === true) {
-                post.liked = false;
-                post.stats.likes--;
-              } else {
-                post.liked = true;
-                post.stats.likes++;
-              }
-              return post;
-            } else {
-              return post;
-            }
-          });
-        case 'delete-tuit':
-          return state.filter(
-              post => post.id !== action.post.id);
-        case 'create-tuit':
-          const newTuit = {
-            post: action.post,
-            id: (new Date()).getTime() + '',
-            postedBy: {
-              "username": "ReactJS"
-            },
-            stats: {
-              retuits: 111,
-              likes: 222,
-              comments: 333
-            },
-            attachments: {
-              video: "",
-              image: "",
-            },
-            avatarimage: "./muskpfp.png",
-            time: "now",
-            liked: false,
-          }
-          return [
-            newTuit,
-            ...state,
-          ];
-        default:
-          return tuits
-      }
-    }
+// const tuitsReducer =
+//     (state = tuits, action) => {
+//       switch (action.type) {
+//         case 'like-tuit':
+//           return state.map(post => {
+//             if(post.id === action.post.id) {
+//               if(post.liked === true) {
+//                 post.liked = false;
+//                 post.stats.likes--;
+//               } else {
+//                 post.liked = true;
+//                 post.stats.likes++;
+//               }
+//               return post;
+//             } else {
+//               return post;
+//             }
+//           });
+//         case 'delete-tuit':
+//           return state.filter(
+//               post => post.id !== action.post.id);
+//         case 'create-tuit':
+//           const newTuit = {
+//             post: action.post,
+//             id: (new Date()).getTime() + '',
+//             postedBy: {
+//               "username": "ReactJS"
+//             },
+//             stats: {
+//               retuits: 111,
+//               likes: 222,
+//               comments: 333
+//             },
+//             attachments: {
+//               video: "",
+//               image: "",
+//             },
+//             avatarimage: "./muskpfp.png",
+//             time: "now",
+//             liked: false,
+//           }
+//           return [
+//             newTuit,
+//             ...state,
+//           ];
+//         default:
+//           return tuits
+//       }
+//     }
+
+import {FIND_ALL_TUITS} from "../actions/tuits-actions";
+
+const tuitsReducer = (state = [], action) => {
+  switch (action.type) {
+    case FIND_ALL_TUITS:
+      return action.tuits;
+    default:
+      return state;
+  }
+}
 
 export default tuitsReducer;
--- src/components/tuiter/tuit-list/tuit-list.js
+++ src/components/tuiter/tuit-list/tuit-list.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 // import tuits from "../data/tuits.json";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector}
+  from "react-redux";
+//import * as service
+//  from '../../../full-stack-developer/services/tuits-service';
+import {findAllTuits} from
+      "../actions/tuits-actions";
 import TuitListItem
   from "./tuit-list-item";
 import WhoToFollowListItem from "../who-to-follow-list/who-to-follow-list-item";
@@ -9,9 +14,25 @@ import WhoToFollowListItem from "../who-to-follow-list/who-to-follow-list-item";
 const TuitList = () => {
   const tuits = useSelector(
       (state) => state.tuits);
-
+  const dispatch = useDispatch();
+  // const findAllTuits = async () => {
+  //   const tuits = await service.findAllTuits();
+  //   dispatch({
+  //     type: 'FIND_ALL_TUITS',
+  //     tuits: tuits
+  //   });
+  // }
+  // useEffect(findAllTuits, []);
+  useEffect(() =>
+          findAllTuits(dispatch),
+      []);
+  // findAllTuits list item below is just an attempt to see if any tuits are being retrieved
+  // at all; the only list item that shows on the site is [object Promise], so probably not
   return (
       <ul className="ttr-tuits list-group">
+        <li>
+          {findAllTuits().toString()}
+        </li>
         {
           tuits.map && tuits.map(post =>
               <TuitListItem key={post.id}
